90f57e86a732baf4ff146d032cc02b8b39fc30e7
KJY <kid4211@gmail.com>
2024-08-04 02:20:41 +0900

    전략 패턴 적용

---
+++ src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,56 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.ContextV2;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 전략 패턴 적용
+ * Context 와 Strategy 를 "선조립" 후 실행하는 방식이 아니라 Context 를 실행할 때 마다 전략을 인수로 전달하는 방식
+ * - 실행할 때 마다 전략을 유연하게 변경할 수 있음
+ * - 단점 역시 실행할 때 마다 전략을 계속 지정해주어야 함
+ */
+@Slf4j
+public class ContextV2Test {
+
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+    }
+
+    /**
+     * 전략 패턴 익명 - 내부 클래스
+     */
+    @Test
+    void strategyV2() {
+        ContextV2 context = new ContextV2();
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+    }
+
+    /**
+     * 전략 패턴 익명 - 내부 클래스(Lambda)
+     */
+    @Test
+    void strategyV3() {
+        ContextV2 context = new ContextV2();
+        context.execute(() -> log.info("비즈니스 로직1 실행"));
+        context.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+}
---
+++ src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV1.java
@@ -0,0 +1,26 @@
+package hello.advanced.trace.strategy.code.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ContextV1 {
+    private final Strategy strategy;
+
+    public ContextV1(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void execute() {
+        // 비즈니스 로직 실행
+        long startTime = System.currentTimeMillis();
+
+        strategy.call(); // 위임
+
+        // 비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+
+        log.info("resultTime={}", resultTime);
+    }
+
+}
